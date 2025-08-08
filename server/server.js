c2831253635a4a1718c0a15779ac002ef072a7a4
Naman Jain <namanjain2004.in@gmail.com>
2025-08-08 22:31:04 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port =4004;
 
 // Serve static files from the public directory
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
